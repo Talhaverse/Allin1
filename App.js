f99b15700b02ceca558a9670bcef87d2e4c89d9b
Syed Talha Ahmed <41064991+Talhaverse@users.noreply.github.com>
2024-01-22 15:49:03 +0500

    .

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Will be working on this open source allin1 reactnative app</Text>
+      <Text>Will be working on this open source allin1 reactnative-app</Text>
       <StatusBar style="auto" />
     </View>
   );
